42ce49b8899e75a49c518681c02a28cd47db1455
Matthew Wilber <mwilber@gmail.com>
2023-02-18 22:04:12 -0500

    Artwork layout adjustments

--- src/components/media-art.js
+++ src/components/media-art.js
@@ -2,6 +2,6 @@ import { html } from '../vendor/@arrow-js/core/index.min.mjs';
 
 export default (data) => 
 	html`
-		<!--<div id="display-artwork" style="background-image: url('${data.artwork}')"></div>-->
-		<img id="display-artwork" src="${data.artwork}" crossorigin="Anonymous" onload="HandleImg(this)" />
+		<div id="display-artwork" style="background-image: url('${data.artwork}')"></div>
+		<img id="display-artwork-ref" src="${data.artwork}" crossorigin="Anonymous" onload="HandleImg(this)" />
 	`;

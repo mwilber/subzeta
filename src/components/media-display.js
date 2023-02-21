71b94aca0ee05765c8626a13fb65d5c06c23d06f
Matthew Wilber <mwilber@gmail.com>
2023-02-20 20:59:50 -0500

    Move search button back.

--- src/components/media-display.js
+++ src/components/media-display.js
@@ -4,18 +4,18 @@ import { searchcircle } from '../icons.js';
 export default (data, loadAlbum, loadArtist) => 
 	html`
 		<div style="grid-area: meta;">
-			<button class="button-search" @click="${() => {
-				const searchSubmit = document.getElementById('search-submit');
-				const searchQuery = document.getElementById('search-query');
-				searchQuery.value = data.artist;
-				searchSubmit.click();
-			}}">
-				${searchcircle}
-			</button>
 			<div id="display-title">${data.title}</div>
 			<div id="display-album"><button @click="${() => {loadAlbum(data.albumId)}}">${data.album}</button></div>
 			<div id="display-artist"> 
 				<button @click="${() => {loadArtist(data.artistId)}}">${data.artist}</button>
+				<button class="button-search" @click="${() => {
+					const searchSubmit = document.getElementById('search-submit');
+					const searchQuery = document.getElementById('search-query');
+					searchQuery.value = data.artist;
+					searchSubmit.click();
+				}}">
+					${searchcircle}
+				</button>
 			</div>
 		</div>
 	`;
